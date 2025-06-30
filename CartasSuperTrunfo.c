41665126b5d7a4f8444d895ce903639aef57dbd4
CoderV25 <202506150241@alunos.estacio.br>
2025-06-30 18:12:37 +0000

    Refatora o cálculo do PIB e adiciona a lógica de Super Poder nas cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,9 +3,10 @@
 int main() {
 
     // Variáveis
-    int popu1, popu2, pontosT1, pontosT2;
-    float area1, area2, pib1, pib2, densip1, densip2;
-    double pibpc1, pibpc2;
+    int pontosT1, pontosT2;
+    unsigned long int popu1, popu2;
+    float area1, area2, densip1, densip2, superPoder1, superPoder2;
+    double pib1, pib2, pibpc1, pibpc2;
     char estado1, estado2, cidade1[20], cidade2[20], cod1[4], cod2[4];
 
     
@@ -33,7 +34,7 @@ int main() {
     scanf("%f", &area1);
 
     printf("Digite o PIB: \n");
-    scanf("%f", &pib1);
+    scanf("%lf", &pib1);
 
     printf("Digite o número de pontos turísticos: \n");
     scanf("%d", &pontosT1);
@@ -57,7 +58,7 @@ int main() {
     scanf("%f", &area2);
 
     printf("Digite o PIB: \n");
-    scanf("%f", &pib2);
+    scanf("%lf", &pib2);
 
     printf("Digite o número de pontos turísticos: \n");
     scanf("%d", &pontosT2);
@@ -73,26 +74,44 @@ int main() {
     //Calculando o PIB per Capita
 
     //Carta 1
-    pibpc1 = (float) (pib1  / popu1) * 1000000000; // PIB em bilhões de reais
+    pibpc1 = (pib1  / popu1) * 1000000000.0; // PIB em bilhões de reais
     //Carta 2
-    pibpc2 = (float) (pib2  / popu2) * 1000000000; // PIB em bilhões de reais
+    pibpc2 = (pib2  / popu2) * 1000000000.0; // PIB em bilhões de reais
 
+    // Calculando o Super Poder (opcional, não foi solicitado, mas incluído para completar a lógica)
+
+    // Carta 1
+    superPoder1 = popu1 + area1 + pib1 + pontosT1 + pibpc1 + (1 / densip1); 
+    // Carta 2
+    superPoder2 = popu2 + area2 + pib2 + pontosT2 + pibpc2 + (1 / densip2);
+    
     // Saída Carta 1
     printf("\n=== Informações da Carta 1 ===\n");
     printf("Estado: %c \nCódigo: %s \nNome da Cidade: %s\n", estado1, cod1, cidade1);
     printf("Área: %.2f km²\n", area1);
-    printf("População: %d \nPIB: %.2f bilhões de reais \nNúmero de Pontos Turísticos: %d\n", popu1, pib1, pontosT1);
+    printf("População: %lu \nPIB: %.2f bilhões de reais \nNúmero de Pontos Turísticos: %d\n", popu1, pib1, pontosT1);
     printf("Densidade Populacional: %.2f hab/km²\n", densip1);
     printf("PIB per Capita: %.2f Reais\n", pibpc1);
+    printf("Super Poder: %.2f\n", superPoder1);
     
     // Saída Carta 2
     printf("\n=== Informações da Carta 2 ===\n");
     printf("Estado: %c \nCódigo: %s \nNome da Cidade: %s\n", estado2, cod2, cidade2);
     printf("Área: %.2f km²\n", area2);
-    printf("População: %d \nPIB: %.2f bilhões de reais \nNúmero de Pontos Turísticos: %d\n", popu2, pib2, pontosT2);
+    printf("População: %lu \nPIB: %.2f bilhões de reais \nNúmero de Pontos Turísticos: %d\n", popu2, pib2, pontosT2);
     printf("Densidade Populacional: %.2f hab/km²\n", densip2);
     printf("PIB per Capita: %.2f Reais\n", pibpc2);
-    
+    printf("Super Poder: %.2f\n", superPoder2);
 
+    // Comparação das Cartas
+    printf("\n=== Comparação das Cartas ===\n");
+    printf("Comparando as Cartas: \n");
+    printf("População: Carta 1 venceu: %d\n", popu1 > popu2);
+    printf("Área: Carta 1 venceu: %d\n", area1 > area2);
+    printf("PIB: Carta 1 venceu: %d\n", pib1 > pib2);
+    printf("Densidade Populacional: Carta 1 venceu: %d\n", densip1 > densip2);
+    printf("PIB per Capita: Carta 1 venceu: %d\n", pibpc1 > pibpc2);
+    printf("Super Poder: Carta 1 venceu: %d\n", superPoder1 > superPoder2);
+    
 return 0;
 }
